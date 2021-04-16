612b5190e8a238e27dc0d1fd6c4475bd1a1c8142
Mohamed H. Mohamed <mohamed@bsmg.co>
2021-04-16 01:14:28 -0500

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -21,7 +21,8 @@ export const Header: FunctionComponent<IHeaderProps> = ({ isMenuOpen, onClick })
       {/*    <Logo />*/}
       {/*  </a>*/}
       {/*</div>*/}
-      <p>
+      <p style={{ fontSize: "1.75em" }} 
+>
         <a href="/" rel="home">
           {`${generalSettingsTitle}`}
         </a>
